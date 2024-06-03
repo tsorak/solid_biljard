137125911dc4b9dcd65b4e31d9587dfd48326ad6
Karl Osterberg <karost0249@gmail.com>
2024-06-03 16:18:39 +0200

    Fetch MOTD with createResource

--- client/src/routes/Motd.jsx
+++ client/src/routes/Motd.jsx
@@ -1,11 +1,18 @@
+import { createResource, Show } from "solid-js";
+
 import logo from "../logo.svg";
 import styles from "./Motd/Motd.module.sass";
+import getMotd from "./Motd/getMotd.js";
 
 export default function Motd() {
+  const [motd] = createResource(getMotd);
+
   return (
     <main class={styles.main}>
       <img src={logo} class={styles._logo} alt="logo" />
-      <p>Foo</p>
+      <Show when={!motd.loading} fallback={() => <p>Loading motd...</p>}>
+        <p>{motd().motd}</p>
+      </Show>
     </main>
   );
 }
--- src/api/motd.rs
+++ src/api/motd.rs
@@ -1,7 +1,15 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse};
+use axum::{extract::State, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Motd {
+    motd: String,
+}
 
 pub async fn motd(State(state): State<crate::State>) -> impl IntoResponse {
-    let body = format!("Message of the day is:\n{}", state.motd);
+    let motd = Motd {
+        motd: format!("Message of the day is:\n{}", state.motd),
+    };
 
-    (StatusCode::OK, body)
+    Json(motd)
 }
